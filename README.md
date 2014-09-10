a4176014626049c3f358a494397f605e1dd2b93f
Jeremie Parker <jparker@guimiesoft.com>
2014-09-10 10:39:51 +0200

    changed version to 0.1.0 - updated readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-TODO: write something meaningful here
+Ported to Meteor from [CryptoJS](https://code.google.com/p/crypto-js/). 
+
+Provide a variety of Cipher Block Mode.
+
+Related package:
+
+- [`jparker:crypto-aes`](https://github.com/p-j/meteor-crypto-aes)
+
+Other CryptoJS package : 
+
+- [`jparker:crypto-md5`](https://github.com/p-j/meteor-crypto-md5)
+- [`jparker:crypto-sha1`](https://github.com/p-j/meteor-crypto-sha1)
+- [`jparker:crypto-sha256`](https://github.com/p-j/meteor-crypto-sha256)
+- [`jparker:crypto-base64`](https://github.com/p-j/meteor-crypto-base64)
+- [`jparker:crypto-hmac`](https://github.com/p-j/meteor-crypto-hmac)
+
+See also
+--------
+The CryptoJS project lives at <https://code.google.com/p/crypto-js/>.
--- package.js
+++ package.js
@@ -1,15 +1,21 @@
 Package.describe({
 	summary: 'Crypto Mode Package for CryptoJS, standard secure algorithms',
-	version: '3.1.2',
+	version: '0.1.0',
 	git: 'https://github.com/p-j/meteor-crypto-mode.git'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
 	api.versionsFrom('METEOR@0.9.1.1');
-	api.use('jparker:crypto-core@3.1.2', ['client', 'server']);
-	api.use('jparker:crypto-cipher-core@3.1.2', ['client', 'server']);
-	api.imply('jparker:crypto-core', ['client', 'server']);
-	api.imply('jparker:crypto-cipher-core', ['client', 'server']);
+
+	api.use([
+    'jparker:crypto-core@0.1.0',
+		'jparker:crypto-cipher-core@0.1.0'
+  ], ['client', 'server']);
+
+	api.imply([
+    'jparker:crypto-core',
+		'jparker:crypto-cipher-core'
+  ], ['client', 'server']);
 
 	api.addFiles([
 		'lib/mode-cfb.js',
@@ -20,9 +26,14 @@ Package.onUse(function(api) {
 	], ['client', 'server']);
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('jparker:crypto-mode');
-  api.use('jparker:crypto-aes');
-  api.addFiles('tests/tests.js');
+Package.onTest(function (api) {
+	api.use([
+    'jparker:crypto-core@0.1.0',
+    'jparker:crypto-cipher-core@0.1.0',
+    'jparker:crypto-mode@0.1.0',
+  	'jparker:crypto-aes@0.1.0',
+  	'tinytest'
+  ], ['client', 'server']);
+
+	api.addFiles('tests/tests.js', ['client', 'server']);
 });
